f3663c88b6ce3eeb6ebc1a5e7f1414e2c8fbaea2
Your New Name <mahamudul408432@gmail.com>
2024-10-28 11:08:48 +0600

    check apis

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use GuzzleHttp\Psr7\Message;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'required',    
+            'price' => 'required',
+        ]);
+
+        $product = Product::create($validatedData);
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        //check if the products exits
+        if($product) {
+            $product->delete();
+            return response()->json(['message' => 'products deleted successfully'], 200);
+        }else{
+            return response()->json(['message' => 'product not found'], 404);
+        }
+    }
+}
